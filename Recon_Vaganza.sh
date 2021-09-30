3068144fd16a00dbdb658b20dcad00ce9cc01684
Andrei Cesmegi <79755533+andreicesmegi@users.noreply.github.com>
2021-09-30 19:51:52 -0300

    Agora sim, funcionando bem.

--- Recon_Vaganza.sh
+++ Recon_Vaganza.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 echo "----------------------------"
 echo "----------------------------"
 echo "  ***  Recon_Vaganza  ***   "
@@ -17,6 +16,7 @@ echo "Press 1 for Lista 1 - 41 itens"
 echo "Press 2 for Lista 2 - Mil itens"
 echo "Press 3 for Lista 3 - 28 itens"
 echo "Press 4 for Lista 4 - 20 mil itens"
+echo "Press 5 para Inserir sua lista .txt"
 echo ""
 read list
 
@@ -28,6 +28,12 @@ elif [ $list == "3" ];then
         Load_list=lista3.txt
 elif [ $list == "4" ];then
         Load_list=lista4.txt
+elif [ $list == "5" ];then
+        echo "insira o nome do seu arquivo txt"
+        read Load_list
+        if [ $Load_list != ".txt" ];then
+                echo "Somente arquivo .txt."
+        fi
 else
         echo "INVALIDO!!!!"
         echo""
@@ -63,7 +69,20 @@ for palavraz in $(cat $Load_list);do
 resp_1=$(curl -s -H "user-agent: Obaoba" -o /dev/null -w "%{http_code}" $site/$palavraz/)
 
 if [ $resp_1 == "200" ];then
-        echo "$site/$palavraz/"
+        echo "+ $site/$palavraz/"
         echo ""
+
+        for arquivo in $(cat $Load_list);do
+        resp_arq=$(curl -s -H "user-agent: Obaoba" -o /dev/null -w "%{http_code}" $site/$palavraz/$arquivo)
+        if [ $resp_arq == "200" ];then
+                echo "+--> $site/$palavraz/$arquivo"
+                echo ""
+        fi
+        done
 fi
 done
+echo ""
+echo "                                     !!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+echo "                                    Au revoir -----------------"
+echo "                                 ----------------------------"
+echo "                               ***  Recon_Vaganza  ***   "
