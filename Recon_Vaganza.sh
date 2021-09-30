9df8fb981b0e9bc810757a21c6cf39e7d31134c7
Andrei Cesmegi <79755533+andreicesmegi@users.noreply.github.com>
2021-09-30 17:19:42 -0300

    Update Recon_Vaganza.sh

--- Recon_Vaganza.sh
+++ Recon_Vaganza.sh
@@ -7,18 +7,47 @@ echo "  ***  Recon_Vaganza  ***   "
 echo "----------------------------"
 echo "----------------------------"
 
-
 echo ""
 echo "Informe o site:"
 read site
+
+echo ""
+echo "Selecione uma lista:"
+echo "Press 1 for Lista 1 - 41 itens"
+echo "Press 2 for Lista 2 - Mil itens"
+echo "Press 3 for Lista 3 - 28 itens"
+echo "Press 4 for Lista 4 - 20 mil itens"
+echo ""
+read list
+
+if [ $list == "1" ];then
+        Load_list=lista1.txt
+elif [ $list == "2" ];then
+        Load_list=lista2.txt
+elif [ $list == "3" ];then
+        Load_list=lista3.txt
+elif [ $list == "4" ];then
+        Load_list=lista4.txt
+else
+        echo "INVALIDO!!!!"
+        echo""
+        exit
+fi
+
+Total=$(wc -w $Load_list | cut -d " " -f 1)
+echo ""
+echo "Esta lista contem $Total palavras."
+
+echo ""
+echo ".... ---> Scanninnnng   :)"
+echo ""
 
 echo "----------------------------"
 echo "    ARQUIVOS ENCONTRADOS    "
 echo "----------------------------"
 
-for palavra in $(cat lista3.txt)
-do
-resp=$(curl -s -H "user-agent: Desectool" -o /dev/null -w "%{http_code}" $site/$palavra)
+for palavra in $(cat $Load_list);do
+resp=$(curl -s -H "user-agent: Obaoba" -o /dev/null -w "%{http_code}" $site/$palavra)
 
 if [ $resp == "200" ];then
         echo "$site/$palavra"
@@ -26,17 +55,15 @@ if [ $resp == "200" ];then
 fi
 done
 
-
 echo "++++++++++++++++++++++++++++++"
 echo "   DIRETORIOS ENCONTRADOS     "
 echo "++++++++++++++++++++++++++++++"
 
-for palavra in $(cat lista3.txt)
-do
-resp_1=$(curl -s -H "user-agent: Desectool" -o /dev/null -w "%{http_code}" $site/$palavra/)
+for palavraz in $(cat $Load_list);do
+resp_1=$(curl -s -H "user-agent: Obaoba" -o /dev/null -w "%{http_code}" $site/$palavraz/)
 
 if [ $resp_1 == "200" ];then
-        echo "$site/$palavra/"
+        echo "$site/$palavraz/"
         echo ""
 fi
 done
